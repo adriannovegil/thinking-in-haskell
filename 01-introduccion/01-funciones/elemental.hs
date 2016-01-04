a26b8b34c1749b8fd54d6c4117cf517194f3b447
Adrian Novegil <adrian.novegil@adesis.com>
2016-01-03 23:58:53 -0600

    Nueva función en Haskell. Cálculo de volumen de una esfera.

--- 01-introduccion/01-funciones/elemental.hs
+++ 01-introduccion/01-funciones/elemental.hs
@@ -30,7 +30,7 @@ sumaMonedas a b c d e = 1 * a + 2 * b + 5 * c + 10 * d + 20 * e
 --
 -- volumenEsfera 10 == 4188.790204786391
 
-
+volumenEsfera r = (4 / 3) * pi * r^3
 
 -- 04 - Área de una corona circular.
 -- -----------------------------------------------------------------------------
