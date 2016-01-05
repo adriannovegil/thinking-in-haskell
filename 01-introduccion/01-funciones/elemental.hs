7f420a751f78a9a8527a53303c14138e33c8c2be
Adrian Novegil <adrian.novegil@gmail.com>
2016-01-04 22:51:38 -0600

    Ejemplo de rotación de listas de elementos.

--- 01-introduccion/01-funciones/elemental.hs
+++ 01-introduccion/01-funciones/elemental.hs
@@ -100,18 +100,18 @@ xor4 x y = x /= y
 -- 8.1 - Definir la función rota1 tal que (rota1 xs) es la lista obtenida
 -- poniendo el primer elemento de xs al final de la lista. Por ejemplo,
 --
--- rota1 [2,5,7,3] == [2,5,7,3]
+-- rota1 [3,2,5,7] == [2,5,7,3]
 
-
+rota1 xs = tail xs ++ [head xs]
 
 -- 8.2 definir la función rota tal que (rota n xs) es la lista obtenida
 -- poniendo los n primeros elemntos de xs al final de la lista. por ejemplo,
 --
--- rota 1 [3,2,5,7] = [2,5,7,3]
--- rota 2 [3,2,5,7] = [5,7,3,2]
--- rota 3 [3,2,5,7] = [7,3,2,5]
+-- rota 1 [3,2,5,7] == [2,5,7,3]
+-- rota 2 [3,2,5,7] == [5,7,3,2]
+-- rota 3 [3,2,5,7] == [7,3,2,5]
 
-
+rota n xs = drop n xs ++ take n xs
 
 -- 09 - Ranggo de una lista.
 -- -----------------------------------------------------------------------------
