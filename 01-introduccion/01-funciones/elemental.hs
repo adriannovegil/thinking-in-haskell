f3a10561fbd89039939cc1293f622119fd1eca2f
Adrian Novegil <adrian.novegil@adesis.com>
2016-01-07 22:37:45 -0600

    Función que calcula el número de soluciones de una ecuación de segundo grado a partir de los coeficientes de la misma.

--- 01-introduccion/01-funciones/elemental.hs
+++ 01-introduccion/01-funciones/elemental.hs
@@ -395,7 +395,11 @@ numeroMayor x y = a * 10 + b
 -- numeroDeRaices 4 4 1 == 1
 -- numeroDeRaices 5 23 12 == 2
 
-
+numeroDeRaices a b c
+    | d < 0     = 0
+    | d == 0    = 1
+    | otherwise = 2
+    where d = b^2 - 4 * a * c
 
 -- 28 - Raíces de las ecuaciones cuadráticas.
 -- -----------------------------------------------------------------------------
