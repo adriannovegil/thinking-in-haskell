17d5fadc7c16735817cf7c79e8a0fc5ff0846ba3
Adrian Novegil <adrian.novegil@adesis.com>
2016-01-07 22:46:51 -0600

    Función que calcula las raíces de las ecuaciones cuadráticas.

--- 01-introduccion/01-funciones/elemental.hs
+++ 01-introduccion/01-funciones/elemental.hs
@@ -409,7 +409,15 @@ numeroDeRaices a b c
 -- raices 1 (-2) 1 == [1.0,1.0]
 -- raices 1 3 2 == [-1.0,-2.0]
 
+raices_1 a b c = [(-b + d) / t, (-b - d) / t]
+    where d = sqrt (b^2 - 4 * a * c)
+          t = 2 * a
 
+raices_2 a b c
+    | d >= 0    = [(-b + e) / (2 * a), (-b - e) / (2 * a)]
+    | otherwise = error "No tiene raices reales"
+    where d = b^2 - 4 * a * c
+          e = sqrt d
 
 -- 29 - Área de un triángulo mediante la formula de Herón.
 -- -----------------------------------------------------------------------------
